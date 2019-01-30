ff21045b795ffee095339bcbca74ca544cd8e98e
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-30 21:29:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key,value|
-    puts holiday_hash.keys[0]
+    ""<< holiday_hash.keys[0]
     
   end
 end
