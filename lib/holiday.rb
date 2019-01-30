cd67a9637c5e20774f49c4401836a57e0bad0e26
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-30 21:12:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.include?(supply)
+holiday_hash.each do |season|
+  return holiday_hash[season]
 end
 
 def all_holidays_with_bbq(holiday_hash)
